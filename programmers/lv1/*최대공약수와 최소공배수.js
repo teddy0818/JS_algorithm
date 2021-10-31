c34b5964a3f54d6454b0dc51370aa69f8cbe462b
Byeongjin Hwang <taddy0818@naver.com>
2021-10-31 11:57:58 +0900

    코드 수정

--- programmers/lv1/*최대공약수와 최소공배수.js
+++ programmers/lv1/*최대공약수와 최소공배수.js
@@ -1,16 +1,17 @@
 /* 유클리드 호제법 */
 
 function solution(n, m) {
-    let resultArr = [];
-    const maxCommonDividor = (a, b) => {
-        if(b == 0) {
-            return a;
+    let result = [];
+    
+    const GreatestCommonFactor = (a, b) => {
+        if(b % a == 0) {
+            return a
         }
-        return maxCommonDividor(b, a % b);
-    };
-    
-    resultArr.push(maxCommonDividor(n, m));
-    resultArr.push(n * m / maxCommonDividor(n, m));
-    
-    return resultArr;
+        
+        return GreatestCommonFactor(b, b % a)
+        
+    }
+    result.push(GreatestCommonFactor(n, m))
+    result.push(n*m / GreatestCommonFactor(n, m))
+    return result;
 }
